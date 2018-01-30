3f3d446e23d8ec4ffc9b53aca01447df19b64142
Charles Ferreira <charles.dev@live.com>
2018-01-30 13:25:09 -0200

    Initial Commit

---
+++ RiseOfARockstar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RiseOfARockstar
+//
+//  Created by Charles Ferreira on 30/01/2018.
+//  Copyright © 2018 Charles Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
